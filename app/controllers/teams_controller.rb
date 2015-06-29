bc4b8244247d19e206178b78084622e958b3e3e9
Alf Alexander <alf1337@gmail.com>
2015-06-29 22:34:54 +0200

    Users can now only add users not in their team to it

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -8,11 +8,22 @@ class TeamsController < ApplicationController
 
   def show
     @team = Team.find(params[:id])
+    if current_user.nil? || !current_user.teams.include?(@team)
+      redirect_to teams_path
+    end
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    users = params[:team][:user_ids] + @team.users.pluck(:id)
+    @team.update_attribute(:user_ids, users)
+    redirect_to @team
   end
 
   def create
     @team = Team.new(team_params)
     if @team.save
+      @team.update_attribute(:user_ids, current_user.id)
       redirect_to root_path, notice: "Laget ditt har blitt opprettet."
     else
       render "new"
