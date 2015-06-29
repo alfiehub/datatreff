bf59bc3bc3a55fa552554c319a054e5f66b06341
Alf Alexander <alf1337@gmail.com>
2015-06-29 10:04:42 +0200

    Create controllers and some actions for user creation/session control

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_username(params[:user][:username))
+
+    if user
+      session[:user_id] = user.id
+    else
+      flash[:danger] = "Brukernavnet eller passordet er ugyldig."
+      render :new
+    end
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, flash: {success: "Du er blitt logget ut!"}
+  end
+end
