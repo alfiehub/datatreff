de18b9fa096df5cdeeff3ac45467be4187369285
Alf Alexander <alf1337@gmail.com>
2015-06-29 10:10:07 +0200

    Add name and username to user model and fix new user form

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20150629075026) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "username"
     t.string   "name"
     t.string   "email"
     t.string   "mobile"
