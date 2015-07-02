042a6a221e76720f560c1911259b0f5751e571e5
Alf Alexander <alf1337@gmail.com>
2015-07-02 22:43:37 +0200

    Bracket with test data is now shown, some CSS override also made

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require "bootstrap"
+//= requre_tree "./bootstrap"
+//= require "jquery-bracket"
--- app/assets/javascripts/bracket.js
+++ app/assets/javascripts/bracket.js
@@ -1,1 +1,26 @@
-alert("IT WORKS!")
+
+$.ajax({
+  url: "matches.json",
+  dataType: 'json',
+  success: function(data) {
+    console.log("POP");
+
+  }
+});
+
+
+var minimalData = {
+teams : [
+          ["Team 1", "Team 2"], /* first matchup */
+        ["Team 3", "Team 4"]  /* second matchup */
+          ],
+        results : [
+          [[1,2], [3,4]],       /* first round */
+        [[4,6], [2,1]]        /* second round */
+          ]
+}
+
+$(function() {
+  $('.bracket').bracket({
+    init: minimalData /* data to initialize the bracket with */ })
+});
