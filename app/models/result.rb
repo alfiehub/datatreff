d7d29ed3fc45c45cb27d4679e735f058ffdea938
Alf Alexander <alf1337@gmail.com>
2015-07-07 11:09:37 +0200

    Add some data validation for images

--- app/models/result.rb
+++ app/models/result.rb
@@ -3,7 +3,10 @@ class Result < ActiveRecord::Base
   belongs_to :competition
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
+  validates :image, :attachment_presence => true
+  validates_with AttachmentPresenceValidator, :attributes => :image
+  validates_with AttachmentSizeValidator, :attributes => :image, :less_than => 20.megabytes
 
   scope :accepted, -> { where(validated: true) }
 
