e401e5ce38cd886e28660077f029e6657d88df2b
Alf Alexander <alf1337@gmail.com>
2015-07-09 22:56:11 +0200

    Add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  EMAIL_REGEX = /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/
   has_secure_password
 
   has_many :user_teams
@@ -9,5 +10,6 @@ class User < ActiveRecord::Base
 
   validates_uniqueness_of :username, :email, :mobile
   validates :username, :name, :mobile, :email, presence: true
+  validates_format_of :email, with: EMAIL_REGEX
   validates :password, presence: true, on: :create
 end
