be6cfe61f767c73ea74fabf1a5de2d54e2170797
Alf Alexander <alf1337@gmail.com>
2015-07-12 00:36:59 +0200

    Delete all boostrap stylesheets and javascripts
    Turns out they werent needed.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require "bootstrap"
-//= requre_tree "./bootstrap"
 //= require "jquery-bracket"
+//= require bootstrap-sprockets
