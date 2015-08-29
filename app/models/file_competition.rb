658063c55cd368ff0b481f09aaa118c59a7f26cd
Alf Alexander <alf1337@gmail.com>
2015-08-29 09:32:49 -0700

    Add FileCompetition data validation and relations

--- app/models/file_competition.rb
+++ app/models/file_competition.rb
@@ -1,2 +1,6 @@
 class FileCompetition < ActiveRecord::Base
+  has_many :file_results
+  has_many :users, through: :file_results
+
+  validates :name, :description, :admin_name, :admin_email, :deadline, presence: true
 end
