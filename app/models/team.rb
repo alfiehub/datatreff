2e4ba97b2b93c21afd571c93f177eb00a4e91e9b
Alf Alexander <alf1337@gmail.com>
2015-09-03 19:44:49 +0200

    Users listed for a team is now sorted after when they joined

--- app/models/team.rb
+++ app/models/team.rb
@@ -11,6 +11,10 @@ class Team < ActiveRecord::Base
     self.users.order('user_teams.created_at ASC').first
   end
 
+  def ordered_users
+    self.users.order('user_teams.created_at ASC')
+  end
+
   def name=(new_name)
     while !TeamSeed.find_by_team_name(self[:name]).nil?
       TeamSeed.find_by_team_name(self[:name]).update_attribute(:team_name, new_name)
