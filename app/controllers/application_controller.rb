a07d1f2d29df5f322d9802d6ab3ebffac3651fa8
Alf Alexander <alf1337@gmail.com>
2015-09-03 14:09:22 -0700

    Add redirect message when redirecting because event hasnt started

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,6 +26,7 @@ class ApplicationController < ActionController::Base
   end
 
   def event_started
+    flash[:success] = "Du kan ikke gjør dette enda, LANet har ikke startet :'("
     redirect_to root_path if !is_admin? && Time.now < Rails.application.config.event_start
   end
 
