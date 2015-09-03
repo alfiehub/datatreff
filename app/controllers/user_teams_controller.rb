1ec6f0018a757006a90c7fe7a752e59d28731a78
Alf <alf1337@gmail.com>
2015-09-03 19:52:52 +0200

    Admins can now remove all users from a team, except for the team captain

--- app/controllers/user_teams_controller.rb
+++ app/controllers/user_teams_controller.rb
@@ -6,6 +6,8 @@ class UserTeamsController < ApplicationController
     user_id = user_team.user_id
     if user_id != team.contact_person.id && (current_user.id == user_id || team.contact_person.id == current_user.id) && current_user.teams.pluck(:id).include?(team_id) && user_team.destroy
       flash[:success] = "Du fjernet et bruker fra laget."
+    elsif user_id != team.contact_person.id && is_admin? && user_team.destroy
+      flash[:success] = "Du fjernet et bruker fra laget."
     else
       flash[:danger] = "Du kan ikke fjerne denne brukeren."
     end
