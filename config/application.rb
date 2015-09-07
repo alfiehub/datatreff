60d3b954dca8890f616375df3435ca05fcd1f56f
Alf Alexander <alf1337@gmail.com>
2015-09-07 23:06:09 +0200

    Restrict available locales

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Datatreff
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.available_locales = [:nb, :en]
     config.i18n.default_locale = :nb
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
