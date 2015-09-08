0d6d1c4972bd6d7d167680eb35af6e6686d4ef69
Alf Alexander <alf1337@gmail.com>
2015-09-08 04:34:13 +0200

    Change the name of :nb localization to :no, Google love

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,8 @@ module Datatreff
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.available_locales = [:nb, :en]
-    config.i18n.default_locale = :nb
+    config.i18n.available_locales = [:no, :en]
+    config.i18n.default_locale = :no
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
