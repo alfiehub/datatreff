9976dac8139eceec1714e1e125f646b156d79ee6
Alf Alexander <alf1337@gmail.com>
2015-09-18 00:11:41 +0200

    Fuck that Insta instance variable, how bout doing it in the view?

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,6 @@
 class SiteController < ApplicationController
   def index
     @pages = Page.front_page.order(:position)
-    @instagram = Instagram.tag_recent_media("galaxelan", {:count => 5})
   end
 
   def chat
