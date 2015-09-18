0ca0b40a69aab21f2cbd044c699eae92f19f7e93
Alf Alexander <alf1337@gmail.com>
2015-09-19 01:48:58 +0200

    Add results_cache_key for proper caching at results#index

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -2,4 +2,8 @@ module ResultsHelper
   def not_accepted_cache_key
     Result.not_accepted.pluck(:created_at).reduce{|p, l| p.to_i + l.to_i}
   end
+
+  def results_cache_key c_id
+    Competition.find_by_id(c_id).results.pluck(:updated_at).reduce{|p, l| p.to_i + l.to_i}
+  end
 end
