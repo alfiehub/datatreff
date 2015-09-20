d2c3d834e36b5d275a9b48f8208a9c4f2418c822
Alf <alf1337@gmail.com>
2015-09-20 14:44:24 +0200

    Update elapsed countdown text

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,7 +30,7 @@ ready = function() {
   $(function() {
     $("#clock").countdown("2015/09/18 16:00:00", {elapse: true}).on('update.countdown', function(event) {
       if (event.elapsed) {
-      $(this).text(event.strftime('GalaxeLAN har startet!'));
+      $(this).text(event.strftime("GalaxeLAN er over for denne gang :'( Nedtelling til neste GalaxeLAN kommer!"));
       } else {
       $(this).html(event.strftime('Bare <b>%-D</b> døgn, <b>%-H</b> %!H:time,timer;, <b>%-M</b> %!M:minutt,minutter; og <b>%-S</b> %!S:sekund,sekunder; igjen!'));
       };
