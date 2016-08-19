52e63c6ab3bebb815141cc5213ddc55332d63431
Alf Alexander <alf1337@gmail.com>
2016-08-19 14:32:23 +0200

    Making stuff ready for the 2016 LAN

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,9 +28,9 @@ ready = function() {
   });
 
   $(function() {
-    $("#clock").countdown("2015/09/18 16:00:00", {elapse: true}).on('update.countdown', function(event) {
+    $("#clock").countdown("2016/09/16 16:00:00", {elapse: true}).on('update.countdown', function(event) {
       if (event.elapsed) {
-      $(this).text(event.strftime("GalaxeLAN er over for denne gang :'( Nedtelling til neste GalaxeLAN kommer!"));
+      $(this).text(event.strftime("GalaxeLAN har begynt!"));
       } else {
       $(this).html(event.strftime('Bare <b>%-D</b> døgn, <b>%-H</b> %!H:time,timer;, <b>%-M</b> %!M:minutt,minutter; og <b>%-S</b> %!S:sekund,sekunder; igjen!'));
       };
--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,8 @@ module Datatreff
     config.active_record.raise_in_transactional_callbacks = true
 
     # Some variables for the website
-    config.event_name = "GalaxeLAN 2015"
-    config.event_start = DateTime.new(2015, 9, 18, 16, 00, 0)
+    config.event_name = "GalaxeLAN 2016"
+    config.event_start = DateTime.new(2016, 9, 16, 16, 00, 0)
 
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
   end
