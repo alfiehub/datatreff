b83ea0dcab011213b0954bd2fb615bb60e2caad5
Alf Alexander <alf1337@gmail.com>
2016-09-15 23:18:42 +0200

    Add instagram config

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,7 @@
+require 'instagram'
+
+Instagram.configure do |config|
+    config.client_id = 'd3dc8ac4a08c4d4ea3732cb60698666c'
+    config.client_secret = '17e7090ef9364154b352879059dca913'
+    config.access_token = '213385796.d3dc8ac.62fb5cc67f1845a487e494ceb2153ae8'
+end
