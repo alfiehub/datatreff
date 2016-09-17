720fd20c5b0bd17d8583dc0e61d8622ebda79163
alf <alf1337@gmail.com>
2016-09-18 01:10:08 +0200

    Add last MIME types for .zip-files

--- app/models/file_result.rb
+++ app/models/file_result.rb
@@ -1,10 +1,11 @@
 class FileResult < ActiveRecord::Base
+  MIME_TYPES = ["application/zip", "application/x-zip", "application/rar-compressed", "application/rar", "application/x-rar-compressed", "application/octet-stream", "multipart/x-zip", "application/x-zip-compressed", "application/x-compressed"]
 
   belongs_to :file_competition
   belongs_to :user
 
   has_attached_file :contribution
-  validates_attachment_content_type :contribution, content_type: ["application/zip", "application/x-zip", "application/rar-compressed", "application/rar", "application/x-rar-compressed", "application/octet-stream"]
+  validates_attachment_content_type :contribution, content_type: MIME_TYPES
   validates :contribution, attachment_presence: true
 
   validates_with AttachmentPresenceValidator, attributes: :contribution
@@ -14,6 +15,6 @@ class FileResult < ActiveRecord::Base
 
   before_post_process :skip_for_zip
   def skip_for_zip
-    ! %w(application/zip application/x-zip application/rar application/x-rar-compressed application/octet-stream).include?(contribution_content_type)
+    ! MIME_TYPES.include?(contribution_content_type)
   end
 end
